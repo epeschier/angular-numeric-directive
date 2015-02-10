a1a3e14df67254029055f1b2be2d2c106e491065
Erik Peschier <erik.peschier@purplefox.nl>
2015-02-10 15:57:06 +0100

    Changed coverage folder location

--- test/numeric.conf.js
+++ test/numeric.conf.js
@@ -41,7 +41,8 @@ module.exports = function(config) {
 
     coverageReporter: {
           type : 'lcov',
-          dir : 'coverage/'
+          dir : 'coverage',
+          subdir: '.'
         },
 
     // web server port
