785575364a98716401e7b15a28d71a0d4449d097
Felipe Klein Zigmundo <felipekzig@users.noreply.github.com>
2017-05-30 16:33:00 -0300

    Fix string conversion for value

--- src/numeric-directive.js
+++ src/numeric-directive.js
@@ -141,7 +141,7 @@
              */
             function numberWithCommas(value) {
                 if (formatting) {
-                    var parts = value.toString().split(decimalSeparator);
+                    var parts = (""+value).split(decimalSeparator);
                     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, groupSeparator);
                     return parts.join(decimalSeparator);
                 }
@@ -174,7 +174,7 @@
                 if (angular.isUndefined(value)) {
                     value = '';
                 }
-                value = value.toString().replace(decimalSeparator, '.');
+                value = (""+value).replace(decimalSeparator, '.');
 
                 // Handle leading decimal point, like ".5"
                 if (value.indexOf('.') === 0) {
@@ -299,7 +299,7 @@
             function onFocus() {
                 var value = ngModelCtrl.$modelValue;
                 if (!angular.isUndefined(value)) {
-                    ngModelCtrl.$viewValue = value.toString().replace(".", decimalSeparator);
+                    ngModelCtrl.$viewValue = (""+value).replace(".", decimalSeparator);
                     ngModelCtrl.$render();
                 }
             }
